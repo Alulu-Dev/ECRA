6aebecd8c05fb6565756fa1b8c829c977e2b7590
Eshtaol Girma Bayessa <it.eshtaol.girma@gmail.com>
2022-03-20 14:55:34 +0300

    user account control add

---
+++ ReceiptChecker/API/core/accountControl/profileControl.py
@@ -0,0 +1,62 @@
+from urllib3.exceptions import ResponseError
+
+from ..model import User
+
+
+def create_new_customer(personal_info):
+    try:
+        print(personal_info)
+        new_customer = User()
+        new_customer.code_name = personal_info['username']
+        new_customer.legal_name = personal_info['full name']
+        new_customer.password = personal_info['password']
+        new_customer.profile_picture = profile_picture
+
+        new_customer.save()
+
+    except ResponseError:
+        return "Data cannot be Created", 500
+
+
+def remove_customer_account(account_id):
+    """
+    * user can remove their account and
+    * admins can remove users when they violate regulations
+    will be updated to change status to deleted and once 30 days pass to actually delete the data
+    """
+    try:
+        account = User.objects.get(id=account_id)
+        account.delete()
+        return True
+
+    except ConnectionError:
+        return "Data cannot be Removed", 500
+
+
+def display_customer_account_details(account_id):
+    try:
+        account = User.objects.get(id=account_id)
+        response = {
+            'username': account.code_name,
+            'official name': account.legal_name,
+            'receipt count': account.receipt_count
+        }
+        return {'user': response}
+    except ResponseError:
+        return "Data Couldn't Be Fetched", 500
+
+
+def update_customer_account_details(account_id, new_data):
+    try:
+        account = accountModel.objects.get(id=account_id)
+
+        if 'username' in new_data and new_data['username'] != account.code_name:
+            account.update(set__code_name= new_data['username'])
+        if 'full name' in new_data and new_data['full name'] != account.legal_name:
+            account.update(ser__legal_name=new_data['full name'])
+        if 'password' in new_data and new_data['password'] != account.password:
+            account.update(set__password=new_data['password'])
+            
+        display_customer_account_details(account_id)
+    except ResponseError:
+        return "Data Couldn't Be Fetched", 500
